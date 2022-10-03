5b33ac26e4daf7268c321e64e88565a15f7f294b
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2022-10-03 23:20:43 +0900

    Create 1978.py

---
+++ 8 기본 수학2/1978.py
@@ -0,0 +1,18 @@
+N = int(input())
+count = 0
+nums = map(int, input().split())
+
+for i in nums:
+    cnt = 0
+    # 1은 소수가 아니라 제외
+    if i == 1:
+        continue
+
+    for j in range(2, i + 1):
+        # cnt=1이면 소수
+        if (i % j == 0):
+            cnt += 1
+    if cnt == 1:
+        count += 1
+
+print(count)
