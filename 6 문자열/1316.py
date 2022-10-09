ebf4606ce83cc000738fb405c9dcd07e56fcf41d
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2022-10-09 21:24:10 +0900

    Update 1316.py

--- 6 문자열/1316.py
+++ 6 문자열/1316.py
@@ -1,8 +1,6 @@
 # https://www.acmicpc.net/problem/1316
 
-alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                 'u', 'v', 'w', 'x', 'y', 'z']
-
+# 입력할 단어 갯수 입력
 N = int(input())
 
 count = 0
@@ -10,16 +8,20 @@ for i in range(0, N):
     # 단어 입력
     word = str(input())
     alphabets = []
-    for alphabet in alphabet_list:
-        # 단어에 있는 알파뱃 저장
-        if word.find(alphabet) >= 0:
-            alphabets += [alphabet]
-    # 저장한 순서대로 삭제 (미구현)
+
+    # 단어의 알파뱃 중복없이 순서대로 저장
+    for alphabet in word:
+        if alphabet not in alphabets:
+            alphabets.append(alphabet)
+
+    # 저장한 알파뱃 순서대로 삭제
     for alphabet in alphabets:
         word = word.lstrip(alphabet)
+
     # 다 삭제되었으면 연속문자 => 카운트 추가
-        if word =='':
-            count +=1
+    if word == '':
+        count += 1
+
 print(count)
 
 
