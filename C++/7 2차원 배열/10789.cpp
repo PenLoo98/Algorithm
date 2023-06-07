eecc2b344563ea091695c810b3bcafe8e812be3d
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-06-08 00:14:57 +0900

    solve 10789.cpp

--- C++/7 2차원 배열/10789.cpp
+++ C++/7 2차원 배열/10789.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     // 5*15 행렬 선언
-    int Matrix[5][15] = {'!',};
+    char Matrix[5][15] = {NULL,};
 
     // 행렬에 문자열 입력받음
     for(int i=0;i<5;i++){
@@ -18,8 +18,10 @@ int main(){
     // 결과값 출력
     for(int i=0;i<15;i++){
         for(int j=0;j<5;j++){
-            if(Matrix[j][i] == '!') continue;
+            if(Matrix[j][i] == NULL) continue;
             else cout << (char)Matrix[j][i];
         }
     }
+    cout << '\n';
+    return 0;
 }
