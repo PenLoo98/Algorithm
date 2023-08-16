be3d9ce54520bddc7a7bf2558af95be51b609b9f
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-08-16 22:03:41 +0900

    solve 2563,25206

--- JAVA/백준/단계별/7. 2차원 배열/B4_2563.java
+++ JAVA/백준/단계별/7. 2차원 배열/B4_2563.java
@@ -8,29 +8,39 @@ public class B4_2563 {
         // 색종이 수 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] axios = new int[100][2];
-
+        // 100*100개의 타일을 boolean 타입으로 생성
+        boolean[][] allArea = new boolean[100][100];
         // 종이 수 만큼 위치 입력
         for(int i=0;i<N;i++){
             // 데이터 타입은 무엇? -> 2차원 배열
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            // 처음입력
 
+            // 해당하는 영역 true 로 변환
+            for(int j=a; j<a+10 & j<allArea.length ;j++){
+                for(int k=b; k<b+10 & k<allArea[j].length ;k++){
+                    allArea[j][k]=true;
+                }
+            }
+        }
+        // true 갯수 세기: 방법1 이중 for문
+        int count = 0;
+        for(int i=0;i<allArea.length;i++){
+            for(int j=0;j<allArea[i].length;j++){
+                if(allArea[i][j]){
+                    count++;
+                }
+            }
         }
 
-        // 겹치는 넓이 = 전체 갯수 넓이 - 겹치는 부분 넓이
-        // 가로,세로 10
-        // 겹치는지 어떻게 확인하지? -> [x 좌표값 차이가 10이하일 때] or [y 좌표값 차이가 10이하일 때]
-        // 겹치는 넓이를 어떻게 구하지? -> 좌표값 차이가 10이하일 때 (10 - 좌표값 차이)
-        // 1번째와 3번째 겹치는 경우 어떻게 데이터를 저장하고 비교해야할까?
-        // (3,7 - 5,2 - 5,7)의 넓이: 100+20+50 = 170 // 40+80+50
+//        // true 갯수 세기: 방법2 Stream
+//        long count = Stream.of(allArea)
+//            .flatMap(row -> IntStream.range(0, row.length).mapToObj(i -> row[i]))
+//            .filter(val -> val)
+//            .count();
 
-        // 3개가 겹치는 경우는 어떻게 해야할까?
-        // 해결책1: 전체넓이-겹치는 넓이 빼기
-        // 해결책2: 겹치는 넓이 더하기 = (10-좌표값 차이)^2
-
-        // 보이는 넓이 구하고 출력
+        // true로 바뀐 넓이 출력
+        System.out.println(count);
     }
 }
