f9c8828d8825cfd1dc2379e99d147a2e8923b917
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-08-25 02:19:59 +0900

    solve 2745 for JAVA

--- JAVA/백준/단계별/8. 일반 수학 1/B1_2745.java
+++ JAVA/백준/단계별/8. 일반 수학 1/B1_2745.java
@@ -3,30 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class YetB1_2745 {
+public class B1_2745 {
     public static void main(String[] args) throws IOException {
         // 인스턴스 생성, 배열생성
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        // N,B 입력
         String N = st.nextToken();
-        StringBuilder sb = new StringBuilder(N);
-        N = sb.reverse().toString();
         int B = Integer.parseInt(st.nextToken());
         br.close();
-        int n = 'A'-55;
 
-        // 진법 변환
         int sum = 0;
-        System.out.println('A'-55);
-
-//        for(int i=0;i<N.length();i++){
-//            char n = N.charAt(i);
-//            if("A"<=n )
-//        }
-
-        // 결과 출력
-//        System.out.println(sum);
+        for(int i=0;i<N.length();i++){
+            char c = N.charAt(i);
+            if('0'<=c & '9'>=c){
+                sum = sum*B + ((int)c - (int)'0');
+            }
+            else {
+                sum = sum*B + ((int)c - (int)'A' + 10);
+            }
+        }
+        System.out.println(sum);
     }
 }
---
+++ JAVA/백준/단계별/8. 일반 수학 1/B2_11005.java
@@ -0,0 +1,7 @@
+public class B2_11005 {
+    public static void main(String[] args) {
+        // 10진법 입력
+
+        //
+    }
+}
