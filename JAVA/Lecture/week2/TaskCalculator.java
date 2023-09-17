0f13db104400297e4fcd8eb4f8c5641bbf520e81
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-09-18 02:53:00 +0900

    Update TaskCalculator.java

--- JAVA/Lecture/week2/TaskCalculator.java
+++ JAVA/Lecture/week2/TaskCalculator.java
@@ -1,20 +1,25 @@
 package lecture;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskCalculator {
-    public static void main(String[] args) {
-//        calculateDays(1000,10,2);
-//        System.out.println(localDateList(10));
-        System.out.println(calculateAmount(28,10,2));
+    public static void main(String[] args) throws Exception {
+        int totalAmount = 400;
+        int startAmount = 10;
+        int increaseAmount = 2;
 
+        int spendDay = calculateDays(totalAmount,startAmount,increaseAmount);
+        List<LocalDate> dateList = localDateList(spendDay);
+        List<String> amountList = calculateStackAmount(spendDay,startAmount,increaseAmount);
+        System.out.println(CalandarTaskText("도커 실습",dateList,amountList));
     }
     
     // 매일 조금씩 증가하는 목표를 설정 후 총 몇일이 걸리는 지 계산
     // 총 분량, 시작 분량, 분량 증가량
-    public static void calculateDays(int targetAmount, int startAmount, int increaseAmout){
+    public static int calculateDays(int targetAmount, int startAmount, int increaseAmout){
         int achived = 0;
         int spendDay = 0;
 
@@ -23,7 +28,8 @@ public class TaskCalculator {
             spendDay++;
             startAmount += increaseAmout;
         }
-        System.out.printf("총 소요시간은 %d일입니다.\n",spendDay);
+//        System.out.printf("총 소요시간은 %d일입니다.\n",spendDay);
+        return spendDay;
     }
 
     // 오늘부터 n일까지의 날짜 계산하여 리스트로 반환
@@ -37,13 +43,33 @@ public class TaskCalculator {
         return dateList;
     }
 
-    // 오늘부터 n일까지 분량의 증가량 리스트
-    public static List<Integer> calculateAmount(int totalDays, int startAmount, int increaseAmout){
+    // 오늘부터 n일까지 분량의 범위 리스트
+    public static List<String> calculateStackAmount(int totalDays, int startAmount, int increaseAmout){
         // (옵션)마지막 날은 총 분량을 넘기는 경우 따로 처리
-        List<Integer> amountList = new ArrayList<>();
+        List<String> amountList = new ArrayList<>();
+        int stack = 1;
+        int amount = startAmount;
         for(int i=0;i<totalDays;i++){
-            amountList.add(startAmount+i*increaseAmout);
+            amountList.add(stack + "~" + (stack+amount) + " (" + amount + ")");
+            stack += amount;
+            amount += increaseAmout;
         }
         return amountList;
+    }
+
+    // 날짜 + 주제 + 분량 합치기
+    public static String CalandarTaskText(String taskname, List<LocalDate> dateList, List<String> amountList) throws Exception {
+        // 입력 오류시 예외처리
+        if(dateList.size() != amountList.size()){
+            throw new IOException();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        // 문자열로 합쳐서 리스트에 저장
+        for(int i=0;i<dateList.size();i++){
+            String input = dateList.get(i) +" "+ taskname +" "+ amountList.get(i)+"\n";
+            stringBuilder.append(input);
+        }
+        String TaskCalandarText = stringBuilder.toString();
+        return TaskCalandarText;
     }
 }
