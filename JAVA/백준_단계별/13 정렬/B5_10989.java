138ebd1a36876d787e855fabbfb356ae6f0d15fc
penloo <76810066+PenLoo98@users.noreply.github.com>
2024-02-02 15:21:11 +0900

    solve 10989 with java

---
+++ JAVA/백준_단계별/13 정렬/B5_10989.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B5_10989 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+        Arrays.sort(arr);
+
+        for(int i=0;i<N;i++){
+            sb.append(arr[i]+"\n");
+        }
+        System.out.println(sb);
+    }
+}
