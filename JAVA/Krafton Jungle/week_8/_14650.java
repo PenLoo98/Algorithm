485a1e78eccab38b789d15efb5d6a63ecda5dd2c
PenLoo98 <sohnchanho@gmail.com>
2024-05-16 17:12:30 +0900

    feat: 14650

---
+++ JAVA/Krafton Jungle/week_8/_14650.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class _14650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int)(2*Math.pow(3, n-2)));
+    }
+}
