07a41aa3cb29852a4ba9eb263efc6138ada3d361
penloo <76810066+PenLoo98@users.noreply.github.com>
2024-09-01 23:46:28 +0900

    feat: 10431

---
+++ JAVA/TIL/_10431.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class _10431 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine());
+            int testNum = Integer.parseInt(st.nextToken());
+            int count = 0;
+            for(int j=0; j<20; j++){
+                if(j==19){
+                    
+                }
+            }
+
+        }
+    }    
+}
